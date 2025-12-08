8375a04a455abf48f97ebe0b708dc032c48adb48
Alberto Garcia <mmhgarcia@gmail.com>
2025-12-08 11:42:10 -0400

    Activa admin panel

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Lista from "./pages/Lista";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
+import Admin from "./pages/Admin";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/lista" element={<Lista />} />
         <Route path="/about" element={<About />} />
+        <Route path="/admin" element={<Admin />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
