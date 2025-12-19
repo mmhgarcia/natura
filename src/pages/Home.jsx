3b08c0c13f3aa42f5938dd2fc1eede97462a3506
Alberto Garcia <mmhgarcia@gmail.com>
2025-12-19 12:15:37 -0400

    add useeffect to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 import productosIniciales from "../data/data.json";
 import { db } from '../lib/db/database.js';
